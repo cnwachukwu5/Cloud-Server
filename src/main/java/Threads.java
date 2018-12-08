85d9a2e59f652172198b885e6e61718db1e8bb83
cyprian <cnwachukwu5@gmail.com>
2018-12-08 03:05:16 -0500

    Updated CloudServer. Multithreaded

--- src/main/java/Threads.java
+++ src/main/java/Threads.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Threads implements Runnable{
 
@@ -14,12 +18,15 @@ public class Threads implements Runnable{
     @Override
     public void run() {
         try {
+            int counter = 0;
+            List<String> dataFromCacheNode = new ArrayList<>();
             while(true){
                 FeedBackMechanism feedBackMechanism = new FeedBackMechanism();
                 PrintWriter output = new PrintWriter(cacheNode.getOutputStream(), true);
-                DataInputStream fromCacheNodes = new DataInputStream(cacheNode.getInputStream());//Read from input
+                BufferedReader fromCacheNodes = new BufferedReader(new InputStreamReader(cacheNode.getInputStream()));//Read from input
 
                 String server_status = feedBackMechanism.currentNodeStatus();
+
                 int status_duration = feedBackMechanism.duration_of_Status();
 
                 System.out.println("Server status: " + server_status);
@@ -29,12 +36,19 @@ public class Threads implements Runnable{
                 long end_Duration_Monitor = 0;
 
                 output.println(server_status+"\n");
-                Thread.sleep(status_duration);
-//                while ((end_Duration_Monitor - start_Duration_Monitor) <= status_duration) {
-//                    output.println(server_status);
-//
-//                    end_Duration_Monitor = System.currentTimeMillis();
-//                }
+
+                //while ((end_Duration_Monitor - start_Duration_Monitor) <= status_duration) {
+                    //output.println(server_status);
+                    String value_Read = fromCacheNodes.readLine();
+
+                    if(!value_Read.equals("Not actual data")){
+                        counter++;
+                    }
+                    System.out.println("Value received: " + value_Read);
+                    dataFromCacheNode.add(value_Read);
+
+                    end_Duration_Monitor = System.currentTimeMillis();
+                //}
             }
 
         }catch (Exception e){
